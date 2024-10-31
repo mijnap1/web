6dd165b5f69c0ad1f40095fa0db47a5201f62884
Ryu Jaehyuk <ryubsmile@gmail.com>
2024-10-31 23:05:32 +0900

    update 18th sponsors 7/7

--- utkcc-fe/data/sponsors-data.ts
+++ utkcc-fe/data/sponsors-data.ts
@@ -15,24 +15,52 @@ export const sponsorData: {
 }[] = [
   // 18th sponsors
   {
-    name: '신전떡볶이 - Church',
-    exp: '한국의 신전을 캐나다에서도 - 신전떡볶이 Church점',
+    name: '신전떡볶이',
+    exp: '한국의 신전을 캐나다에서도 - 신전떡볶이 @ Church',
     imageSrc: '/assets/images/sponsors/sinjeon.jpeg',
-    websiteUrl: 'http://linkin.bio/sinjeon_downtown',
+    websiteUrl: 'https://www.instagram.com/sinjeon_church_toronto/',
     locationUrl: 'https://maps.app.goo.gl/gJrk8rDooscHXAtz8',
   },
   {
     name: '우주분식',
-    exp: 'Woojoo Bunsik - Korean Street Food,',
+    exp: 'Woojoo Bunsik - Korean Street Food @ Downtown',
     imageSrc: '/assets/images/sponsors/woojoobunsik.jpeg',
     websiteUrl: 'https://www.instagram.com/woojoobunsik/',
     locationUrl: 'https://maps.app.goo.gl/rz4ZLsD5Sp99Sibc7',
   },
   {
-    name: 'Kanari Restaurant & Bar',
+    name: 'Kanari',
     exp: 'Contemporary Asian Bar',
     imageSrc: '/assets/images/sponsors/kanari.jpeg',
     websiteUrl: 'https://www.instagram.com/kanari.toronto',
     locationUrl: 'https://maps.app.goo.gl/FptEr6oN9tXtPhG19',
+  },
+  {
+    name: 'Hoki Poke',
+    exp: 'Taste of Hawaii - Queen st. W',
+    imageSrc: '/assets/images/sponsors/hokipoke.jpeg',
+    websiteUrl: 'https://www.instagram.com/hokipoketoronto/',
+    locationUrl: 'https://maps.app.goo.gl/oFnPZBEVCpYZku479',
+  },
+  {
+    name: 'Chibap',
+    exp: 'Korean Fried Chicken',
+    imageSrc: '/assets/images/sponsors/chibap.jpeg',
+    websiteUrl: 'https://www.instagram.com/thechibap/',
+    locationUrl: 'https://maps.app.goo.gl/komhCAVePn8i9udW7',
+  },
+  {
+    name: '한국관',
+    exp: 'Korean Village Restaurant',
+    imageSrc: '/assets/images/sponsors/koreanvillage.jpeg',
+    websiteUrl: 'https://www.instagram.com/koreanvillageto/',
+    locationUrl: 'https://maps.app.goo.gl/gDD2g7eFbdD64xJk6',
+  },
+  {
+    name: 'BBQ Spadina',
+    exp: 'Real Crispy, Real Korean Fried Chicken',
+    imageSrc: '/assets/images/sponsors/bbq.jpeg',
+    websiteUrl: 'https://www.instagram.com/bbqchicken_utspadina/',
+    locationUrl: 'https://maps.app.goo.gl/gDD2g7eFbdD64xJk6',
   },
 ];
