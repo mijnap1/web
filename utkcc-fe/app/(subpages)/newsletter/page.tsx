743b70cd947748c019e87342a32f4d7c9ba58f03
mijnap1 <mijnap1@users.noreply.github.com>
2026-09-02 12:30:49 -0400

    Update newsletter PDF and page count

--- utkcc-fe/app/(subpages)/newsletter/page.tsx
+++ utkcc-fe/app/(subpages)/newsletter/page.tsx
@@ -12,7 +12,7 @@ const PdfViewer = dynamic(() => import('@/components/pdfViewer'), {
 });
 
 export default function Newsletter() {
-  const TOTAL_PAGES = 22;
+  const TOTAL_PAGES = 16;
   const fileUrl = '/assets/pdf/newsletter.pdf';
 
   const [currentPage, setCurrentPage] = useState(1);
